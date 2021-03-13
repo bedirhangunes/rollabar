d541ebfa85a1c1f62a040cfd0e7af1b6f80e4fde
Bedirhan Güneş <bahoz.6192@gmail.com>
2021-03-13 21:29:13 +0300

    roll a bar

---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    Rigidbody fizik;
+    public int hiz;
+    int sayac = 0;
+    public int toplanacakObjeSayisi;
+    public Text sayacText;
+    public Text oyunBittiText;
+
+
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay,0,dikey);
+
+        fizik.AddForce(vec * hiz);
+    }
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "engel")
+        {
+            other.gameObject.SetActive(false);
+            sayac++;
+            sayacText.text = "Sayac=" + sayac;
+
+
+            if (sayac == toplanacakObjeSayisi)
+            {
+                oyunBittiText.text = "OYUN BİTTİ";
+            }
+        }
+        
+    }
+}
